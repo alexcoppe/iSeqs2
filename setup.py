463b022af161dd78e31988eae4ec79035e0e17c1
Ale <alexcoppe@gmail.com>
2019-11-19 12:50:12 +0100

    Created build_leukemia_genes_list directory. Moved the build_leukemia_genes_list.py there. Fixed version in setup.py

--- setup.py
+++ setup.py
@@ -5,13 +5,13 @@ import setuptools
 
 setuptools.setup(
      name='iSeqs2',  
-     version='0.1.1',
+     version='0.0.1',
      author="Alessandro Coppe",
      author_email="",
      description="Another bunch of scripts I use in my Next Generation Sequencing Bioinformatics Analyses",
      url="https://github.com",
      packages=["iseqs2"],
-     scripts=["scripts/build_leukemia_genes_list.py"],
+     scripts=["scripts/build_leukemia_genes_list/build_leukemia_genes_list.py"],
      classifiers=[
          "Programming Language :: Python :: 3",
          "License :: OSI Approved :: MIT License",
