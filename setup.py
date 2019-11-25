8c5b36445b0b04495d73a8044cd3c7fa157d69e8
Ale <alexcoppe@gmail.com>
2019-11-25 23:01:15 +0100

    Added filter_vcf_by_gene_names.py script

--- setup.py
+++ setup.py
@@ -9,9 +9,10 @@ setuptools.setup(
      author="Alessandro Coppe",
      author_email="",
      description="Another bunch of scripts I use in my Next Generation Sequencing Bioinformatics Analyses",
-     url="https://github.com",
+     url="https://github.com/alexcoppe/iSeqs2",
      packages=["iseqs2"],
-     scripts=["scripts/build_leukemia_genes_list/build_leukemia_genes_list.py"],
+     scripts=["scripts/build_leukemia_genes_list/build_leukemia_genes_list.py",
+         "scripts/filter_vcf_by_gene_names/filter_vcf_by_gene_names.py"],
      classifiers=[
          "Programming Language :: Python :: 3",
          "License :: OSI Approved :: MIT License",
