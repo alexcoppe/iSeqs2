c6d2d208e449fd7e9a55b242c7290616b6f5c910
Ale <alexcoppe@gmail.com>
2019-11-27 22:56:51 +0100

    Added the --throw option

--- scripts/filter_vcf_by_gene_names/filter_vcf_by_gene_names.py
+++ scripts/filter_vcf_by_gene_names/filter_vcf_by_gene_names.py
@@ -4,12 +4,13 @@ import argparse
 import os.path
 import sys
 
-def build_genes_set(genes_file):
+def build_genes_set(genes_file, to_upper = True):
     """Returns a set of genes from a simple file with a gene in every row
 
     Parameters:
 
     genes_file (string): the file containing one gene per line
+    to_upper (boolean): return all uppercase gene names
 
 
     Returns:
@@ -26,6 +27,8 @@ def build_genes_set(genes_file):
         for line in genes_file.readlines():
             splitted_line = line.split()
             gene = splitted_line[0]
+            if to_upper == True:
+                gene = gene.upper()
             genes.append(gene)
 
     genes = set(genes)
@@ -59,13 +62,16 @@ def main():
     parser.add_argument('-g', '--genes', action='store', type=str, help="A file with one gene name per line", required=True)
     parser.add_argument('-v', '--vcf', action='store', type=str, help="The VCF to be filtered", required=True)
     parser.add_argument('-m', '--meta', action='store', type=str, help="Show or not show meta-information", required=False, default="T")
+    parser.add_argument('-t', '--throw', action='store', type=str, help="Throw away genes present in the file indicated by -g", required=False, default="F")
     args = parser.parse_args()
 
     genes = args.genes
     vcf = args.vcf
     meta = args.meta
+    throw = args.throw
 
     genes_to_keep_or_trash = build_genes_set(genes)
+    print(genes_to_keep_or_trash)
     
     with open(vcf) as vcf:
         for line in vcf.readlines():
@@ -82,9 +88,15 @@ def main():
                     info = splitted_line[7]
                     genes = get_genes_from_info(info)
                     for gene in genes:
-                        if gene in genes_to_keep_or_trash:
-                            print(line[:-1])
-                            break
+                        gene = gene.upper()
+                        if throw == "F":
+                            if gene in genes_to_keep_or_trash:
+                                print(line[:-1])
+                                break
+                        else:
+                            if gene not in genes_to_keep_or_trash:
+                                print(line[:-1])
+                                break
     return 1
 
 if  __name__ == "__main__":
